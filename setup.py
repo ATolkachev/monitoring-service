3fb4c4fad74f874b38173841c071dddef24c104f
Alexander Tolkachev <atolkachev@AT-MacBook-Pro-5.local>
2017-12-07 00:46:29 +0300

    Fix module

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name="MonitorService",
     version="0.1",
     packages=find_packages(),
-    scripts=['monitor.rest.py','monitor.checker.py'],
+    scripts=['monitor/rest.py','monitor/checker.py'],
     zip_safe=True,
     entry_points = {
         'console_scripts': [
