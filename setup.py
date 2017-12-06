1ef2b5c51d1eeb78150fe5f111b4d289d9c3b098
Alexander Tolkachev <atolkachev@AT-MacBook-Pro-5.local>
2017-12-07 00:26:07 +0300

    Another egg

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     version="0.1",
     packages=find_packages(),
     scripts=['monitor.rest.py','monitor.checker.py'],
+    zip_safe=True,
     entry_points = {
         'console_scripts': [
         	'monitor-rest=monitor.rest:main',        # сервис с REST интерфейсом
