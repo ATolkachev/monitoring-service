2ebc41b8e1afd803f77dec7a5ad0bd29eb37661c
Alexander Tolkachev <atolkachev@AT-MacBook-Pro-5.local>
2017-12-07 01:14:51 +0300

    Remove old stuff

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup, find_packages
 setup(
-    name="MonitorService",
-    version="0.1",
+    name="monitor",
+    version="0.1.1",
     packages=find_packages(),
-    scripts=['monitor/rest.py','monitor/checker.py'],
     zip_safe=True,
     entry_points = {
         'console_scripts': [
