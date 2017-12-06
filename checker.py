adc65c619c211141f9b682e693cdff6b0f4d8734
Alexander Tolkachev <atolkachev@AT-MacBook-Pro-5.local>
2017-12-06 23:28:55 +0300

    First release

---
+++ checker.py
@@ -0,0 +1,80 @@
+import asyncio
+import telnetlib
+from time import time
+from pymongo import MongoClient
+
+class CheckerService():
+
+    _monitors = []
+
+    def tic(self):
+        return '%1.3f seconds' % (time() - self._start_time)
+
+    def __init__(self):
+        self.client = MongoClient('127.0.0.1', 27017)
+        self.db = self.client['monitoring']
+        self.alert_collection = self.db['alerts']
+        self.monitor_collection = self.db['monitor']
+
+        monitor_count=self.load_monitors(self.monitor_collection)
+        if(monitor_count==0):
+            print("There is no monitors in Database")
+        else:
+            print("{} monitors loaded.".format(monitor_count))
+
+    def load_monitors(self,collection):
+        data = self.monitor_collection.find()
+
+        for obj in data:
+            monitor = {"id": int(obj['id']), "port": int(obj["port"]), "address": obj['address'], "alive": obj['alive']}
+            self._monitors.append(monitor)
+
+        return len(self._monitors)
+
+    def start_monitors(self):
+        if(len(self._monitors)>0):
+            loop = asyncio.new_event_loop()
+            #ioloop = asyncio.get_event_loop()
+            #asyncio.wait(wait_tasks)
+            asyncio.set_event_loop(loop)
+            loop.run_until_complete(self.run_monitors())
+            loop.close()
+        else:
+            print("There is no loaded monitors.")
+
+    async def run_monitors(self):
+        tasks = []
+        for item in self._monitors:
+            #await asyncio.gather(self.monitor_item(item))
+            tasks.append(self.monitor_item(item))
+        #await asyncio.wait(tasks)
+
+        await asyncio.gather(*tasks)
+
+    async def monitor_item(self, item):
+        response = ''
+        status = True
+        while True:
+            self._start_time = time()
+            connector = asyncio.open_connection(host=item['address'], port=item['port'])
+            try:
+                await asyncio.wait_for(connector,timeout=0.3)
+                response = 'Success'
+            except:
+                status = False
+                response = 'Failed'
+            finally:
+                print("Monitor {}: Test {}:{} - {} in {}".format(item['id'],item['address'],item['port'],response,self.tic()))
+                self.update_monitor(monitor=item, status=status)
+                connector.close()
+            await asyncio.sleep(5)
+
+    def update_monitor(self, monitor, status):
+        update_time = int(time())
+        if monitor['alive'] != status:
+            self.monitor_collection.find_one_and_update({'id': monitor['id']}, {'$set': {"alive": status, 'since': update_time}})
+            monitor['alive'] = status
+
+if __name__ == '__main__':
+    check = CheckerService()
+    check.start_monitors()
--- main.py
+++ main.py
@@ -1,63 +1,161 @@
+import json
+from time import time
 from aiohttp import web
+from pymongo import MongoClient
 
-def get_all_checks():
+class RestService():
 
-    return "Nothing"
+    _max_monitor_id = 0
 
-def get_check_status(id):
-    result = ''
-    if(id<1):
-        result = get_all_checks()
-    else:
-        f = open("checks/{0}.log".format(id),"r")
-        result = f.readline()
-        f.close()
+    def __init__(self):
+        self.client = MongoClient()
+        self.client = MongoClient('127.0.0.1', 27017)
+        self.db = self.client['monitoring']
+        self.alert_collection = self.db['alerts']
+        self.monitor_collection = self.db['monitor']
 
-    return result
+        self.get_max_monitor_id()
 
-def print_check(id):
-    # result = ''
-    # if id == -1:
-    #     result = "All checks"
-    # else:
-    result = get_check_status(id)
+    def get_max_monitor_id(self):
+        max_dict = self.monitor_collection.find().sort([("id", -1)]).limit(1)
 
-    return result
+        for doc in max_dict:
+            self._max_monitor_id = int(doc['id'])
+
+        return self._max_monitor_id
+        # sort({_id:-1}).limit(1).pretty()
+
+    def get_monitor(self, id):
+        return self.monitor_collection.find_one({"id": id})
+
+    def get_status(self, monitor_id):
+        return self.alert_collection.find_one({"monitor_id": monitor_id})
+
+    def get_alert(self, id):
+        return self.alert_collection.find_one({"_id": id})
+
+    def insert_monitor(self, monitor):
+        result = self.monitor_collection.insert_one(monitor)
+
+        return monitor["id"]
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.client.close()
+
+    def get_all_checks(self):
+        result = []
+        data = self.monitor_collection.find({})
+        for check in data:
+            result.append(self.prepare_monitor(check))
+
+        return result
+
+    def get_check_status(self,id):
+        result = ''
+        if(id<1):
+            result = {"items": self.get_all_checks()}
+        else:
+            result = self.prepare_monitor(self.get_monitor(id))
+
+        return result
+
+    def print_check(self,id):
+        # result = ''
+        # if id == -1:
+        #     result = "All checks"
+        # else:
+        result = self.get_check_status(id)
+
+        return result
+
+    def check_insert_monitor(self, insert_data):
+        result = dict()
+
+        if "address" in insert_data:
+            result["address"]=insert_data["address"]
+        else:
+            raise Exception("Please provide address")
+
+        if "name" in insert_data:
+            result["name"] = insert_data["name"]
+        else:
+            raise Exception("Please provide name")
+
+        if "port" in insert_data:
+            if isinstance(insert_data.get("port"), int) and insert_data["port"]>0 and insert_data["port"]<65536:
+                result["port"]=insert_data["port"]
+            else:
+                raise Exception("Error: Wrong port")
+        else:
+            raise Exception("Please provide port number")
+
+        result["id"]=self.get_max_monitor_id()+1
+        result["alive"] = False
+        result["since"] = time()
+        result["enabled"] = True
+
+        return result
+
+    async def handle(self,request):
+        name = request.match_info.get('name', "Anonymous")
+        text = "Hello, " + name
+        return web.Response(text=text)
+
+    async def handle_get(self,request):
+        id = int(request.match_info.get('id', -1))
+        text=str(self.print_check(id))
+        return web.Response(text=text)
+
+    async def handle_post(self,request):
+        data = await request.json()
+        id = -1
+        try:
+            id = self.insert_monitor(self.check_insert_monitor(data))
+        except:
+            return web.Response(status=500, text="Something going wrong.")
+        print("New monitor Added with ID={}".format(id))
+        text = str({"id": id})
+        return web.Response(text=text)
+
+    async def handle_put(self,request):
+        name = request.match_info.get('id', "Anonymous")
+        text = "Hello, " + name
+        return web.Response(text=text)
+
+    async def handle_delete(self,request):
+        name = request.match_info.get('id', "Anonymous")
+        text = "Hello, " + name
+        return web.Response(text=text)
+
+    def prepare_monitor(self, base_monitor):
+        result = dict()
+
+        result["address"] = base_monitor["address"]
+        result["name"] = base_monitor["name"]
+        result["port"] = int(base_monitor["port"])
+        result["id"] = int(base_monitor["id"])
+        result["alive"] = base_monitor["alive"]
+        result["since"] = int(base_monitor["since"])
+
+        return result
+    def main(self):
+
+        #s = self.get_monitor(1)
+
+        #print(s)
+
+        app = web.Application()
+        app.router.add_get('/', self.handle)
+        app.router.add_get('/endpoints', self.handle_get)
+        app.router.add_get('/endpoints/', self.handle_get)
+        app.router.add_post('/endpoints/', self.handle_post)
+        app.router.add_get('/endpoints/{id}', self.handle_get)
+        app.router.add_put('/endpoints/{id}', self.handle_put)
+        app.router.add_delete('/endpoints/{id}', self.handle_delete)
 
 
-async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
+        web.run_app(app, host='127.0.0.1', port=8080)
 
-async def handle_get(request):
-    id = int(request.match_info.get('id', -1))
-    text=print_check(id)
-    return web.Response(text=text)
-
-async def handle_post(request):
-    name = request.match_info.get('id', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
-
-async def handle_put(request):
-    name = request.match_info.get('id', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
-
-async def handle_delete(request):
-    name = request.match_info.get('id', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
-
-app = web.Application()
-app.router.add_get('/', handle)
-app.router.add_get('/endpoints', handle_get)
-app.router.add_get('/endpoints/', handle_get)
-app.router.add_post('/endpoints/', handle_post)
-app.router.add_get('/endpoints/{id}', handle_get)
-app.router.add_put('/endpoints/{id}', handle_put)
-app.router.add_delete('/endpoints/{id}', handle_delete)
-
-
-web.run_app(app, host='127.0.0.1', port=8080)
+if __name__ == '__main__':
+    rest = RestService()
+    rest.main()
