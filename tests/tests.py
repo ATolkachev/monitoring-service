5f9496c1bd7d4a5ffcc42d2c20d94d79fc5e4d4d
Alexander Tolkachev <atolkachev@AT-MacBook-Pro-5.local>
2017-12-15 03:08:56 +0300

    Fix tests

--- tests/tests.py
+++ tests/tests.py
@@ -13,9 +13,11 @@ class TestMethods(unittest.TestCase):
         self.rest = rest.RestService()
 
         check_dict = {"name": "Foo", "address": "127.0.0.1", "port": 80, "alive": False, "since": int(time()), "enabled": True, "id": 1}
+        check_dict_clean = {"name": "Foo", "address": "127.0.0.1", "port": 80, "alive": False, "since": int(time()), "id": 1}
+
 
         self.assertDictEqual(self.rest.check_insert_monitor(check_dict),check_dict)
-        self.assertDictEqual(self.rest.prepare_monitor(check_dict),check_dict)
+        self.assertDictEqual(self.rest.prepare_monitor(check_dict),check_dict_clean)
         self.assertTrue(self.rest.get_max_monitor_id() == 0)
 
         self.rest.insert_monitor(check_dict)
