78f5fabe659392412808424620e3dc0f3251e165
Alexander Tolkachev <atolkachev@AT-MacBook-Pro-5.local>
2017-12-15 03:06:12 +0300

    Fix tests

--- tests/tests.py
+++ tests/tests.py
@@ -15,7 +15,7 @@ class TestMethods(unittest.TestCase):
         check_dict = {"name": "Foo", "address": "127.0.0.1", "port": 80, "alive": False, "since": int(time()), "enabled": True, "id": 1}
 
         self.assertDictEqual(self.rest.check_insert_monitor(check_dict),check_dict)
-
+        self.assertDictEqual(self.rest.prepare_monitor(check_dict),check_dict)
         self.assertTrue(self.rest.get_max_monitor_id() == 0)
 
         self.rest.insert_monitor(check_dict)
